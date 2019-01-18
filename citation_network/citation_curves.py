aa216b3f7da25a15e796b82e0ed5423da037d7c1
Torsten <torsten.heinrich@posteo.net>
2019-01-18 23:59:00 +0000

    implemented new computation and visualization in citation_curves.py

--- citation_network/citation_curves.py
+++ citation_network/citation_curves.py
@@ -1,7 +1,7 @@
 
 import pickle
-import bisect
-import pdb
+#import bisect
+#import pdb
 import numpy as np
 import pandas as pd
 import os
@@ -23,127 +23,109 @@ class CitationCurveSet():
         self.voluntaryOnly = voluntaryOnly
         if self.voluntaryOnly:
             self.citationCurveFileName = "citation_curves_voluntary.pkl"
-            #self.citationListFileName = "received_citation_list_voluntary.pkl"
-            #self.citationCountFileName = "received_citation_count_voluntary.pkl"
-            #self.citationListFileNameCleaned = "received_citation_list_voluntary_cleaned.pkl" 
+            self.citationCurveTLenFileName = "citation_curves_total_lengths_voluntary.pkl"
+            self.citationCurveMatrixFileName = "citation_curves_voluntary.npz"
+            self.citationCurveMatrixKeysFileName = "citation_curves_voluntary_keys.pkl"
         else:
             self.citationCurveFileName = "citation_curves.pkl"
-            #self.citationListFileName = "received_citation_list.pkl"
-            #self.citationCountFileName = "received_citation_count.pkl"
-            #self.citationListFileNameCleaned = "received_citation_list_cleaned.pkl" 
-        """loading datasets"""
-        print("Loading citation lists...")
-        assert os.path.exists(self.citationCurveFileName)
-        with open(self.citationCurveFileName, "rb") as rfile:
-            self.citation_curves = pickle.load(rfile)
-
-        print("Loading dates lists...")
-        self.pddf = pd.read_pickle("patents_dates_years.pkl")      # should have: "applied date", "granted date", "applied year", "granted year"
-        
+            self.citationCurveTLenFileName = "citation_curves_total_lengths.pkl"
+            self.citationCurveMatrixFileName = "citation_curves.npz"
+            self.citationCurveMatrixKeysFileName = "citation_curves_keys.pkl"
+                            
         self.separation = {}
-        self.totallengths = {}
         
-        # populate totallengths
-        last_granted_date = max(pddf["granted date"])
-        self.totallengths = [td.days for td in (last_granted_date - pddf["granted date"][510:521])]
-        self.max_totallength = lax(self.totallengths)
-        
-        # make citation curves into sparse matrix
-        self.citation_curve_matrix = None
-        self.populate_citation_curve_matrix()
-        
-        # get keys
-        self.citation_curves_keys = list(citation_curves.keys())
-        ## selections with
-        #sep_true_false = np.in1d(self.citation_curves_keys, keys_to_be_selected)   # gives bool list
-        #selection_members = np.where(sep_true_false)[0]
-        #selection_nonmembers = np.where(sep_true_false == False)[0]
-        
-        # free memory
-        del self.citation_curves
-        
-        #"""parsing citation curves"""
-        
-        # self.citation_curves = {}
-        
-        # if to_be_cleaned:
-        #    print("Done. Now removing unparsable elements: ")
-        #    self.remove_unparsable()
-        # print("Done. Now parsing: ")
-        # self.parse_curves()
-        # print("Done.")
+        if os.path.exists(self.citationCurveMatrixFileName):
+            print("Data sets found. Loading... ")
+            assert os.path.exists(self.citationCurveTLenFileName)
+            assert os.path.exists(self.citationCurveMatrixKeysFileName)
+            with open(self.citationCurveTLenFileName, "rb") as rfile:
+                self.tlen = pickle.load(rfile)
+            with open(self.citationCurveMatrixKeysFileName, "rb") as rfile:
+                self.tlen = pickle.load(rfile)
+            self.citation_curve_matrix = sp.sparse.load_npz(self.citationCurveMatrixFileName) 
+
+        else:
+            
+            print("Loading citation lists...")
+            assert os.path.exists(self.citationCurveFileName)
+            with open(self.citationCurveFileName, "rb") as rfile:
+                self.citation_curves = pickle.load(rfile)
+
+            print("Loading dates lists...")
+            self.pddf = pd.read_pickle("patents_dates_years.pkl")      # should have: "applied date", "granted date", "applied year", "granted year"
+            
+            print("Removing empty entries from citation curves...")
+            for key in self.citation_curves.keys():
+                if len(self.citation_curves[key]) == 0:
+                    del self.citation_curves[key]
+            
+            print("Obtaining keys...")
+            self.citation_curves_keys = np.asarray(list(self.citation_curves.keys()))
+            print("Cleaning and sorting dates list...")
+            self.pddf = self.pddf.loc[self.citation_curves_keys]
+            self.pddf = self.pddf.loc[self.pddf["granted date"].notnull()]
+            self.pddf = self.pddf.sort_values(by=["granted date"], ascending=False)
+            print("Obtaining cleaned keys...")
+            self.citation_curves_keys = np.asarray(self.pddf.index)
+            
+            print("Populating citation curve length records...")
+            last_granted_date = max(pddf["granted date"])
+            self.tlen = np.asarray([td.days for td in (last_granted_date - pddf["granted date"])])
+            self.maxlen = lax(self.tlen)
+            
+            print("Converting citation curves into sparse matrix...")
+            self.citation_curve_matrix = None
+            self.populate_citation_curve_matrix()
+            
+            print("Freeing memory...")
+            del self.citation_curves
+            
+            print("Saving citation curves as sparse matrix and saving associated records...")
+            sp.sparse.save_npz(self.citationCurveMatrixFileName, self.citation_curve_matrix)
+            with open(self.citationCurveMatrixKeysFileName, "wb") as wfile:
+                pickle.dump(self.citation_curves_keys, wfile, protocol=pickle.HIGHEST_PROTOCOL)
+            with open(self.citationCurveTLenFileName, "wb") as wfile:
+                pickle.dump(self.tlen, wfile, protocol=pickle.HIGHEST_PROTOCOL)
+                
+        assert self.citation_curve_matrix[0] == len(self.tlen) == len(self.citation_curves_keys)
+        print("Sanity check succeeded. All set up.")
     
     def populate_citation_curve_matrix(self):
-        self.citation_curve_matrix = sp.dok_matrix((len(self.citation_curve_matrix), self.max_totallength), dtype=np.int8)
-        for idx, times in enumerate(self.citation_curves.values()):
+        self.citation_curve_matrix = sp.dok_matrix((len(self.citation_curve_matrix), self.maxlen), dtype=np.int8)
+        for idx, key in enumerate(self.citation_curves_keys):
+            times = self.citation_curves[key]
             inttimes =  [td.days for td in times]
             self.citation_curve_matrix[idx, inttimes] = 1
 
         self.citation_curve_matrix = self.citation_curve_matrix.tocsr()
         
     
-    def remove_unparsable(self):
-        volunt_yes_no = {True: "_voluntary", False: ""}
-
-        keys = self.received_citation_list.keys()
-        print("   Keys acquired")
-        difference = list(pd.Index(keys).difference(self.pddf.index))
-        print("   Difference set acquired")
-        #with open("received_citation_list_difference" + volunt_yes_no[self.voluntaryOnly] + ".pkl", "wb") as wfile:
-        #    pickle.dump(difference, wfile, protocol=pickle.HIGHEST_PROTOCOL)
-
-        """ dict comprehension literally takes forever, see other option below """
-        #new_dict = {k: v for k, v in self.received_citation_list.items() if v not in difference}
-        """ dict manipulation is quick """
-        i_max = len(difference)
-        for i, ckey in enumerate(difference):
-            print("Parsed {0} out of {1}".format(i+1, i_max), end="\r")
-            if ckey in self.received_citation_list:
-                del self.received_citation_list[ckey]
-        
-        print("   New dict created. Saving...")
-        with open("received_citation_list" + volunt_yes_no[self.voluntaryOnly] + "_cleaned.pkl", "wb") as wfile:
-            pickle.dump(self.received_citation_list, wfile,  protocol=pickle.HIGHEST_PROTOCOL)
-        
-    def parse_curves(self):
-        lastdate = max([pd.to_datetime(0) if len(rcc)==0 else rcc[-1] for _, rcc in self.received_citation_list.items()])
-        i_max = len(self.received_citation_list.keys())
-        for i, cckey in enumerate(self.received_citation_list.keys()):
-            print("parsing {0} of {1}".format(i+1, i_max), end="\r")
-            startdate = self.pddf["granted date"][cckey]
-            ccurve = np.asarray([(date - startdate).days for date in self.received_citation_list[cckey]])
-            self.citation_curves[cckey] = ccurve
-            self.enddates[cckey] = (lastdate - startdate).days
-        self.maxlen = max([pd.to_timedelta(0).days if len(cc)==0 else cc[-1] for _, cc in self.citation_curves.items()])
-    
-    def save_cc(self):
-        print("Saving...")
-        with open(self.citationCurveFileName, "wb") as wfile:
-            pickle.dump([self.citation_curves, self.enddates], wfile, protocol=pickle.HIGHEST_PROTOCOL)
-        print("Done.")
-        
-    def separate_group(self, memberIDs, criterion):
-        print("Separating...")
-        self.separation[criterion] = {}
-        self.separation[criterion]["members"] = memberIDs
-        non_memberIDs = [ID for ID in self.citation_curves.keys() if ID not in memberIDs]
-        self.separation[criterion]["nonmembers"] = non_memberIDs
-        print("Done.")
-
-    def draw_citation_curve(self, criterion, quantile_low=0.25, quantile_high=0.75):
+    def draw_citation_curve(self, criterion, memberIDs, quantile_low=0.25, quantile_high=0.75):
         print("Computing graphs")
         xs = np.arange(self.maxlen)
+
+        # separate matrix
         mseries = {}
-        mseries["members"] = [self.citation_curves[k] for k in self.separation[criterion]["members"] if k in self.citation_curves]
-        mseries["nonmembers"] = [self.citation_curves[k] for k in self.separation[criterion]["nonmembers"] if k in self.citation_curves]
-        member_median = self.get_citation_quantiles(mseries["members"], .5, xs)
-        member_mean = self.get_citation_mean(mseries["members"], xs)
-        member_low = self.get_citation_quantiles(mseries["members"], quantile_low, xs)
-        member_high = self.get_citation_quantiles(mseries["members"], quantile_high, xs)
-        nonmember_median = self.get_citation_quantiles(mseries["nonmembers"], .5, xs)
-        nonmember_mean = self.get_citation_mean(mseries["nonmembers"], xs)
-        nonmember_low = self.get_citation_quantiles(mseries["nonmembers"], quantile_low, xs)
-        nonmember_high = self.get_citation_quantiles(mseries["nonmembers"], quantile_high, xs)
+        tlens = {}
+        sep_true_false = np.in1d(self.citation_curves_keys, memberIDs)   # gives bool list
+        selection_members = np.where(sep_true_false)[0]
+        selection_nonmembers = np.where(sep_true_false == False)[0]
+        mseries["members"] = self.citation_curve_matrix[selection_members]
+        mseries["nonmembers"] = self.citation_curve_matrix[selection_nonmembers]
+        tlens["members"] = self.tlen[selection_members]
+        tlens["nonmembers"] = self.tlen[selection_nonmembers]
+        
+        # compute moments
+        member_mean, member_std, (member_low, member_median, member_high) = get_csr_quantiles_and_mean(
+                                                                    mseries["members"], 
+                                                                    tlens["members"], 
+                                                                    [0.25, 0.5, 0.75], 
+                                                                    xs)
+        nonmember_mean, nonmember_std, (nonmember_low, nonmember_median, nonmember_high) = get_csr_quantiles_and_mean(
+                                                                    mseries["nonmembers"], 
+                                                                    tlens["nonmembers"], 
+                                                                    [0.25, 0.5, 0.75], 
+                                                                    xs)
         print("Done.")
 
         # Plot and save
@@ -170,31 +152,39 @@ class CitationCurveSet():
         #plt.show()
         print("Done.")
 
-    def get_citation_quantiles(self, ser, quantile, xs):
-        cq = np.asarray([np.quantile([bisect.bisect(s, x) for s in ser], quantile) for x in xs])
-        return cq
-        
-    def get_citation_mean(self, ser, xs):
-        cm = np.asarray([np.mean([bisect.bisect(s, x) for s in ser]) for x in xs])
-        return cm
+    def get_csr_quantiles_and_mean(self, csr_matx, tlen_matx, quantiles, xs):
+        qs = {q: [] for q in quantiles}
+        means = []
+        stds = []
+        for x in xs:
+            # sum matrix to right
+            alive = np.argmax(tlen_matx > x)
+            rowsums = citation_curve_matrix_csr[self.alive:,:x].sum(axis=1) # submatrix with rows > index(first alive at time x) and columns < (index x)
+            # compute quantiles over this
+            for q in quantiles:
+                qs[q].append(np.quantile(rowsums, q))
+                means.append(np.mean(rowsums))        
+                stds.append(np.std(rowsums))        
+        # transform qs and means to ndarray
+        for key in qs.keys():
+            qs[key] = np.asarray(qs[key])
+        means = np.asarray(means)
+        stds = np.asarray(stds)
+        return means, stds, qs
 
-    def get_citation_mean(self, ser, xs):
-        csd = np.asarray([np.std([bisect.bisect(s, x) for s in ser]) for x in xs])
-        return csd
     
-    def get_observation_numbers(self, keys, xs):
-        #ends = [cc[-1] for key, cc in self.citation_curves.items() if key in keys]
-        end_dates = [self.enddates[key] for key in keys]
-        ends = list(end_dates)
-        ends = ends.sort()
-        return np.asarray([bisect.bisect_right(ends, x) for x in xs])
+    #def get_observation_numbers(self, keys, xs):
+    #    #ends = [cc[-1] for key, cc in self.citation_curves.items() if key in keys]
+    #    end_dates = [self.enddates[key] for key in keys]
+    #    ends = list(end_dates)
+    #    ends = ends.sort()
+    #    return np.asarray([bisect.bisect_right(ends, x) for x in xs])
 
 if __name__ == "__main__":
     for vol in [False, True]:
         print("Commencing voluntary={0}...".format(vol))
         """prepare citation curves and save"""
         CCS = CitationCurveSet(vol)    
-        CCS.save_cc()
         
         """load greenness patterns"""
         selections = {}
@@ -208,8 +198,7 @@ if __name__ == "__main__":
         
         """draw central moments and dispersion for green and brown crossectional ensembles"""
         for separ in ["keyword_shapira", "GI_envtech", "GI_IPC"]:
-            CCS.separate_group(selections[separ], separ)
-            CCS.draw_citation_curve(separ)
+            CCS.draw_citation_curve(separ, selections[separ])
 
     """Exit"""
     exit(0)
