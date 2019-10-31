ae19690aac788fac9fba82192cd20a2454693b73
Torsten <torsten.heinrich@posteo.net>
2019-10-31 22:53:47 +0000

    regressions module test version

--- regressions/regressions.py
+++ regressions/regressions.py
@@ -5,87 +5,126 @@ from statsmodels.datasets import utils as du
 import pandas as pd
 import numpy as np
 import pdb
+import sys
 
-def fixed_effect_logit(df, y_var, x_var, fe_var):
-    pdb.set_trace()
-    df["green"] = df["green"].astype('float')
-    df["green"] = df["green"].astype('Int32')
-    #df["green"] = np.int64(df["green"])
-    # get unique values
-    unique_values = list(df[fe_var].unique())
-    fe_var_list = []
-    for i in range(1, len(unique_values)):
-        uv = unique_values[i]
-        df[fe_var + "_" + str(uv)] = np.int64(df[fe_var]==uv)
-        fe_var_list.append(fe_var + "_" + str(uv))
-    df = df[y_var+x_var+fe_var_list]
-    df = df.dropna(axis=0)
-    data = du.Dataset(data=df, names=list(df.columns), endog=df[y_var], exog=df[x_var+fe_var_list])
-    pdb.set_trace()
-    data.exog = sm.add_constant(data.exog, prepend=False)
-    #logitmodel = sm.GLM(data.endog, data.exog, family=sm.families.Binomial(link=statsmodels.genmod.families.links.logit))
-    logitmodel = sm.GLM(data.endog, data.exog, family=sm.families.Binomial(), link=statsmodels.genmod.families.links.logit)
-    gaussianmodel = sm.GLM(data.endog, data.exog, family=statsmodels.genmod.families.Gaussian())
-    return logitmodel, gaussianmodel
-    #res = model.fit()
-    #return res
-
-def analysis(data):
-    # need to transform dataset??
-    ##data = sm.datasets.get_rdataset("dietox", "geepack").data
-    if False:
-        md = smf.mixedlm("pagerank ~ green", data, groups=data["granted_year"])
-        # fails with: IndexError: index 6221293 is out of bounds for axis 0 with size 6026927
-        mdf = md.fit()
-        print(mdf.summary())
-    #pdb.set_trace()
-    ml, mg = fixed_effect_logit(data, ["pagerank"], ["green"], "granted_year")
-    mlf = ml.fit()
-    mgf = mg.fit()
-    print(mlf.summary())
-    print(mgf.summary())
-    md = smf.mixedlm("forward_citations ~ green", data, groups=data["granted_year"])
-    mdf = md.fit()
-    print(mdf.summary())
-    ml, mg = fixed_effect_logit(data, ["forward_citations"], ["green"], "granted_year")
-    mlf = ml.fit()
-    mgf = mg.fit()
-    print(mlf.summary())
-    print(mgf.summary())
+class RegressionSetup():
+    def __init__(self):
+        self.df = pd.read_pickle("CPC_sorted_green_patents_combined_df.pkl")
+        self.df.rename(columns={'Pagerank (all)': 'pagerank', \
+                                'Received citations (all)': 'forward_citations', \
+                                'Pagerank (assignee citations only)': 'pagerank_AssigneeOnly', \
+                                'Received (assignee citations only)': 'forward_citations_AssigneeOnly', \
+                                'granted year': 'granted_year'}, inplace=True)
+        self.fitting_results_detail = {}
+        self.fitting_results = {}
+        self.fitting_failures = {}
+        with open("fitting_results.txt", "w") as wfile:
+            wfile.write("")
+        
+    def run_regression(self, scheme, regressiontype, y_var, x_var, fe_var):
+        """interpret scheme"""
+        greenness_Envtech = True if "Envtech" in scheme else False
+        greenness_IPCGI = True if "IPCGI" in scheme else False
+        greenness_Y_Codes = True if "Y_Codes" in scheme else False
+        self.df["green"] = (self.df["Envtech"]==greenness_Envtech) & (self.df["IPCGI"]==greenness_IPCGI) \
+                                                                        & (self.df["Y_Codes"]==greenness_Y_Codes)
+        if scheme == "Any":
+            self.df["green"] = (self.df["Envtech"]==True) | (self.df["IPCGI"]==True) | (self.df["Y_Codes"]==True)
+        
+        """make formula"""
+        if not isinstance(x_var, (frozenset, list, set, tuple,)):
+            x_var = [x_var]
+        if not isinstance(fe_var, (frozenset, list, set, tuple,)):
+            fe_var = [fe_var]
+        try:
+            assert not isinstance(y_var, (frozenset, list, set, tuple,))
+        except:
+            pdb.set_trace()
+        formula = "{0:s} ~ ".format(y_var)
+        firstelement = True
+        for element in x_var:
+            if firstelement:
+                formula += " {0:s} ".format(element)
+                firstelement = False
+            else:
+                formula += "+ {0:s} ".format(element)
+        for element in fe_var:
+            formula += "+ C({0:s}) ".format(element)
+        
+        print("Running " + formula)
+        
+        """compile filename for saving individual regression result"""
+        filename = "fit_{0:s}_greenness_{1:s}_{2:s}".format(scheme, regressiontype, formula).\
+                    replace("~", "=").replace(" ", "")#.replace("(", "\(").replace(")", "\)")
+        
+        with open(filename, "w") as wfile:
+            wfile.write("")
+        
+        """run regression"""
+        if regressiontype == "logit":
+            regression_function = smf.logit
+        elif regressiontype == "ols":
+            regression_function = smf.ols
+        else:
+            print("Error: Regression type not implemented: {0:s}".format(regressiontype))
+        try:
+            model = regression_function(formula=formula, data=self.df)
+            model_fit = model.fit()
+        except:
+            exc_info = str(sys.exc_info())
+            self.fitting_failures[filename] = exc_info
+            print(exc_info)
+            return
+        
+        """generate prediction iff we have only year dummy and only greenness x"""
+        prediction_df = None
+        if fe_var == ["granted_year"] and x_var == ["green"]:
+            prediction_df = pd.DataFrame({'green':[True]*(2019-1976), 'granted_year': np.arange(1976,2019)})
+            prediction_df["prediction"] = model_fit.predict(prediction_df[["green", "granted_year"]])
+        
+        """save and reload fit while removing data"""
+        model_fit.save(filename, remove_data=True)
+        import statsmodels.iolib.smpickle
+        model_fit = statsmodels.iolib.smpickle.load_pickle(filename)
+        self.fitting_results_detail[filename] = model_fit
+        self.fitting_results[filename] = {"greenness_scheme": scheme,
+                                             "type": regressiontype,
+                                             "formula": formula,
+                                             "filename": filename,
+                                             "y": y_var,
+                                             "x": x_var,
+                                             "fe": fe_var,
+                                             "coefficient": model_fit.params.loc["green[T.True]"],
+                                             "sign": np.sign(model_fit.params.loc["green[T.True]"]).astype(int),
+                                             "p_value": model_fit.pvalues.loc["green[T.True]"],
+                                             "prediction": prediction_df
+                                             }
+        
+        """write fit to text file"""
+        with open("fitting_results.txt", "a") as wfile:
+            wfile.write(str(model_fit.formula) + "\n" + str(model_fit.summary()) + "\n\n")
     
-
-def test():
-    df = pd.DataFrame({"pagerank": [0.01, 0.01, 0.09, 0.01, 0.11, 0.01, 0.02, 0.011, 0.01, 0.06, 0.12, 0.015],
-                       "forward_citations": [1, 1, 12, 1, 10, 2, 1, 1, 1, 4, 10, 1],
-                       "green": [True, True, False, False, True, False, False, False, False, True, True, False],
-                       "granted_year": [1977, 1977, 1977, 1977, 1977, 1978, 1978, 1978, 1978, 1978, 1978, 1978]})
-    #analysis(df)
-    df = pd.DataFrame({"pagerank": [0.01, 0.01, 0.09, 0.01, 0.11, 0.01, 0.02, 0.011, 0.01, 0.06, 0.12, 0.015],
-                       "forward_citations": [1, 1, 12, 1, 10, 2, 1, 1, 1, 4, 10, 1],
-                       "green": [True, False, True, False, True, False, False, False, False, True, True, False],
-                       "granted_year": [1977, 1977, 1977, 1977, 1977, 1978, 1978, 1978, 1978, 1978, 1978, 1978]})
-    analysis(df)
+    def save(self):
+        with open("fitting_results_detail.pkl", "wb") as wfile:
+            pickle.dump(self.fitting_results_detail, wfile, protocol=pickle.HIGHEST_PROTOCOL)
+        with open("fitting_results.pkl", "wb") as wfile:
+            pickle.dump(self.fitting_results, wfile, protocol=pickle.HIGHEST_PROTOCOL)
+        with open("fitting_failures.pkl", "wb") as wfile:
+            pickle.dump(self.fitting_failures, wfile, protocol=pickle.HIGHEST_PROTOCOL)
     
-    
-def load():
-    """To avoid copying dataframes, we do not drag them through different functions."""
-    df = pd.read_pickle("CPC_sorted_green_patents_combined_df.pkl")
-    df.rename(columns={'Pagerank (all)': 'pagerank', 'Received citations (all)': 'forward_citations', 'granted year': 'granted_year'}, inplace=True)
-    df["green"] = df["Envtech"]
-    #analysis(pddf)
-    #ml, mg = fixed_effect_logit(data, ["pagerank"], ["green"], "granted_year")
-    y_var, x_var, fe_var = ["pagerank"], ["green"], "granted_year"
-    #df["green"] = df["green"].astype('float')
-    #df["green"] = df["green"].astype('Int32')
-    #pdb.set_trace()
-    model = smf.logit(formula="{0:s} ~ {1:s} + C({2:s})".format(y_var[0], x_var[0], fe_var), data=df)
-    mf = model.fit()
-    print(mf.summary())
-    #gaussian
-    model = smf.ols(formula="{0:s} ~ {1:s} + C({2:s})".format(y_var[0], x_var[0], fe_var), data=df)
-    mf = model.fit()
-    print(mf.summary())
+    def run(self):
+        schemes = ["Envtech", "IPCGI", "Y_Codes", "Envtech_IPCGI", "IPCGI_Y_Codes", "Y_Codes_Envtech", \
+                                                                            "Envtech_IPCGI_Y_Codes", "Any"]
+        #y_vars = ["pagerank", "forward_citations"]
+        y_vars = ["pagerank", "forward_citations", '5_year_lag_citations', '10_year_lag_citations', \
+                '20_year_lag_citations', '30_year_lag_citations', 'urban', 'officialtype', 'private', \
+                "pagerank_AssigneeOnly", "forward_citations_AssigneeOnly"]
+        for scheme in schemes:
+            for y_var in y_vars:
+                self.run_regression(scheme, 'logit', y_var, ["green"], ["granted_year"])
+                self.run_regression(scheme, 'ols', y_var, ["green"], ["granted_year"])
+        self.save()
     
 if __name__ == "__main__":
-    #test()
-    load()
+    RS = RegressionSetup()
+    RS.run()
